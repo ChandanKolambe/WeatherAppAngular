0b28795ee26980fe21c50b3ba1869f73d1385d86
ChandanKolambe <chandan.kolambe@knorex.com>
2020-05-23 22:36:18 +0530

    Updating Readme file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiKey: 'c479624cf0b29b977f7dace1b41f0644',
-  apiUrl: 'http://api.openweathermap.org/data/2.5'
+  apiUrl: 'https://api.openweathermap.org/data/2.5'
 };
